5b3dddd407773d2ca1bb234f09f7f4d99aaf5ef3
Iqbaladudu <iqbal.adudu@gmail.com>
2025-12-13 00:46:02 +0200

    done

--- backend/app/core/model.py
+++ backend/app/core/model.py
@@ -5,4 +5,4 @@ from ultralytics import YOLO
 current_dir = Path(__file__).parent
 # Load the model from the same directory
 model_path = current_dir / "YOLOv8_Small_RDD.pt"
-model = YOLO(str(model_path))
+model = YOLO("https://pub-0ccce103f38e4902912534cdb3973783.r2.dev/YOLOv8_Small_RDD.pt")
